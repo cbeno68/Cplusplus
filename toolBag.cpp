a2fa4295425c3b0508dea18d4cbd2fc91fe9edd2
cbeno68 <chrisbeno692@gmail.com>
2022-08-31 20:21:32 -0400

    Add files via upload

--- toolBag.cpp
+++ toolBag.cpp
@@ -5,23 +5,64 @@ using namespace std;
 void lotteryGen(){
 	int num[5];
 	srand(time(0));
-
+	
+	cout << "Your winning numbers are:";
 	for(int i=0; i<5;i++){	
-		num[i] = rand()%100; cout << num[i] << ' ';
+		num[i] = rand()%100; 
+		cout << num[i] << ' ';
 	}
 	return;
 }
+void d20(){
+	int output;
+	srand(time(0));
+	
+	output = rand()%20;
+	cout << "You have rolled:" << output << "\n";
+
+	return;
+}
+void d6(){
+	int output;
+	srand(time(0));
+	
+	output = rand()%6;
+	cout << "You have rolled:" << output << "\n";
+
+	return;
+}
 int main(){
-	string select;
+	int select;
 	cout << "***** Welcome to the Tool Bag *****\n";
 	cout << "***** Please select a tool to use *****\n";
 	cout << "** 1.Lottery Number Generator **\n";
+	cout << "** 2.d20 Roll  **\n";	
+	cout << "** 3.d6 Roll  **\n";
+	cout << "** 99. To Exit the Program **\n";
 	cin >> select;
-	if(select == "1"){
+
+	switch(select){
+	
+	case 1:
 	lotteryGen();
-	}
-	else{
-	cout << "Please select a tool.";
+	break;
+
+	case 2:
+	d20();
+	break;
+	
+	case 3:
+	d6();
+	break;
+
+	case 99:
+	
+	break;
+
+	default:
+	cout << "Please make a valid selection.\n";
+	cout << "\n";
+	main();
 	}
 	return 0;
 
